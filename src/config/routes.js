62f286be1abcea7f8b72afb3945642995afc5482
Cameron <mccallie.cameron@gmail.com>
2014-08-20 23:05:50 -0400

    adding server stuff to separate branch

---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Cameron on 8/20/14.
+ */
+
+var rest = require('../server/controllers/rest');
+
+module.exports = function(app) {
+    //app.get('/questions', rest.findAll);
+    //app.get('/questions/:id', rest.findOne);
+    //app.post('answer/', rest.add);
+}
