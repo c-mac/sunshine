5fd246a6fa71f0886c32674795720685d7ac21d6
Cameron <mccallie.cameron@gmail.com>
2014-09-01 22:46:37 -0400

    api routes working

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,12 +1,14 @@
 /**
  * Created by Cameron on 8/18/14.
  */
+
+// For now, let's keep this simple by using just a single response (not an array)
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var answerSchema = new Schema({
     question: String,
-    responses: [{responseName : String}],
+    response: String,
     user: String,
     date: Date
 });
--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@
 
 
 /** WORKING SERVER CODE **/
-var path = require("path"),
-    indexPath = path.join(__dirname, '../');
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(indexPath)).listen(1337);
+//var path = require("path"),
+//    indexPath = path.join(__dirname, '../');
+//var connect = require('connect');
+//var serveStatic = require('serve-static');
+//connect().use(serveStatic(indexPath)).listen(1337);
 
 
 
@@ -58,52 +58,64 @@ connect().use(serveStatic(indexPath)).listen(1337);
 
 /** Building basic REST API using: http://scotch.io/tutorials/javascript/build-a-restful-api-using-node-and-express-4#comment-section **/
 
-//var express = require('express'),
-//    app = express(),
-//    bodyParser = require('body-parser'),
-//    port = process.env.PORT || 1337,
-//    router = express.Router(),
-//    mongoose = require('mongoose'),
-//    config = require('./../mongo.json'),
-//    Answer = require('./models/answer');
-//
-//
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
-//
-//
-//router.use(function(req, res, next) {
-//    console.log("SOMETHING IS HAPPENING.");
-//    next();
-//});
-//
-//router.get('/', function(req, res) {
-//    res.json({ message: 'hooray! welcome to our API'});
-//});
-//
-//
-//router.route('/answer')
-//
-//    .post(function(req, res) {
-//        var answer = new Answer();
-//        answer.question = req.body.question;
-//        answer.responses.push(req.body.input_response);
-//        answer.user = req.body.user;
-//        answer.date = new Date();
-//
-//        answer.save(function(err) {
-//            if (err) {
-//                res.send(err);
-//            }
-//
-//            res.json({ message: 'Answer created!'});
-//        });
-//    });
-//
-//app.use('/api', router);
-//
-//app.listen(port);
-//console.log('Magic happens on port ' + port);
-//
-//var connectionString = 'mongodb://' + config.user + ':' + config.password + '@ds049558.mongolab.com:49558/cmac-sample';
-//mongoose.connect(connectionString);
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 1337,
+    router = express.Router(),
+    mongoose = require('mongoose'),
+    config = require('./../mongo.json'),
+    Answer = require('./models/answer');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+router.use(function(req, res, next) {
+    console.log("SOMETHING IS HAPPENING.");
+    next();
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our API'});
+});
+
+router.route('/answers')
+
+    .get(function(req, res) {
+        Answer.find(function(err, answers) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(answers);
+        });
+    });
+
+
+router.route('/answer')
+
+    .post(function(req, res) {
+        var answer = new Answer();
+        answer.question = req.body.question;
+        answer.response = req.body.input_response;
+        answer.user = req.body.user;
+        answer.date = new Date();
+
+        answer.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json({ message: 'Answer created!'});
+        });
+    });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+var connectionString = 'mongodb://' + config.user + ':' + config.password + '@ds049558.mongolab.com:49558/cmac-sample';
+mongoose.connect(connectionString);
